0b66e90283dd00c68ae5eb9759a54b3a8a483850
Abraham <abrahamosazee3@gmail.com>
2022-06-02 06:28:32 -0700

    fix code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
-#!/bin/bash
-#include <studio.h>
+#include <stdio.h>
+
+/**
+ * main - return the integal of the function
+ *
+ * Description:use to print "programming is like building a mutilingual puzzle"
+ *
+ * Return: value of 0
+ *
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
