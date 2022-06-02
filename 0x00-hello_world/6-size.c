8808f025d64b9e1b5719d4935afdde39306685f5
Abraham <abrahamosazee3@gmail.com>
2022-06-02 07:53:04 -0700

    fix codes errors

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - return integal of 0
+ *
+ * Description: print the size of diffirent characters and words
+ *
+ * Return: Always succes return o
+ *
+ */
+
+int main(void)
+{
+	printf("Size of char is: %ld byte(s)\n", sizeof(char));
+	printf("size of int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)", sizeof(float));
+
+	return (0);
+}
