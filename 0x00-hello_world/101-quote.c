7586b9b843938d163abb11ef8916a6b227ba0001
Abraham <abrahamosazee3@gmail.com>
2022-06-02 11:41:41 -0700

    fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - return the integer value of 1
+ *
+ * Description: printing to the standard error file stream
+ *
+ * Return: value 1
+ *
+ */
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar
+		, 2015-10-19");
+return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-	printf("Size of a char is: %ld byte(s)", sizeof(char));
-	printf("Size of an int is: %ld byte(s)", sizeof(int));
-	printf("Size of a long int: %ld bytes(s)", sizeof(long int));
-	printf("Size of a long long int: %ld bytes(s)", sizeof(long long int));
-	printf("Size of a float: %ld bytes(s)", sizeof(float));
+	printf("Size of a char is: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int is: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s\n)", sizeof(float));
 
 	return (0);
 }
