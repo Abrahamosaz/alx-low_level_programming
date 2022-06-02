01d2034a56c6561f8e9cf46e110bfb42996bb3f2
Abraham <abrahamosazee3@gmail.com>
2022-06-02 13:11:40 -0700

    using fprintf to print out a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - return the integer value of 1
+ * main - use to return the integer value 1
  *
- * Description: printing to the standard error file stream
+ * Description: use to print to standard output in the main function
  *
- * Return: value 1
+ * Return: return the value 1
  *
  */
 
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar
-		, 2015-10-19");
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }
