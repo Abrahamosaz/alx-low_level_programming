a0385e3c74b3001b94e78998c393a7a5edeeebd2
Abraham <abrahamosazee3@gmail.com>
2022-06-07 15:34:48 -0700

    add teo numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_last_digit - print out the last digit of a number
+ * @x: value that the function perform operation on
+ *
+ * Description: print out the required results
+ *
+ * Return: return the integer value 0
+ *
+ */
+int print_last_digit(int x)
+{
+	int j;
+	int k;
+
+	if (x = 0)
+	{
+		j = _putchar(x);
+		return (j);
+	} else if (x > 0)
+	{
+		k = _putchar(x);
+		return (k);
+	} else 
+		return ();
+}
