27a6b78a64e40c3ae87186152c51d19e367393d7
Abraham <abrahamosazee3@gmail.com>
2022-06-07 09:08:51 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,13 +13,13 @@ int _islower(int c)
 	char check;
 
 	check = tolower(c);
-	if (c == check)
-	{
-		return (1);
-	}
 	if (c != check || c == 0 || c == 20)
 	{
 		return (0);
+	}
+	if (c == check)
+	{
+		return (1);
 	}
 
 	return (0);
