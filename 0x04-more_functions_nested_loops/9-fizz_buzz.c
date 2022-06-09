b48822f89fc7206e003ed5b8b65771ad300401c2
Abraham <abrahamosazee3@gmail.com>
2022-06-09 15:12:12 -0700

    fix error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,34 +10,34 @@ char f_w[] = "Fizz", s_w[] = "Buzz", t_w[] = "FizzBuzz";
 
 for (i = 1; i < 101; i++)
 {
-if (i > 100)
-	break;
-if (i % 3 == 0 && i % 5 == 0)
+if (i == 1)
+	_putchar(i + '0');
+if (i > 1 && i % 3 == 0 && i % 5 == 0)
 {
+	_putchar(' ');
 	for (h = 0; h < 8; h++)
 		_putchar(t_w[h]);
-	_putchar(' ');
-} else if (i % 3 == 0)
+} else if (i > 1 && i % 3 == 0)
 {
+	_putchar(' ');
 	for (j = 0; j < 4; j++)
 		_putchar(f_w[j]);
-	_putchar(' ');
-} else if (i % 5 == 0)
+} else if (i > 1 && i % 5 == 0)
 {
+	_putchar(' ');
 	for (k = 0; k < 4; k++)
 		_putchar(s_w[k]);
-	_putchar(' ');
 } else
 {
-	if (i < 10)
-		_putchar(i + '0');
 	_putchar(' ');
-	if (i > 10)
+	if (i > 1 && i < 10)
+		_putchar(i + '0');
+	if (i > 1 && i > 10)
 	{
+		_putchar(' ');
 		_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 	}
-	_putchar(' ');
 }
 }
 _putchar('\n');
