16e060106ee09e7cefb345d57ed99a4dc49567d1
Abraham <abrahamosazee3@gmail.com>
2022-06-09 06:00:04 -0700

    print lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,20 +11,14 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int v;
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (j < 10)
-				_putchar(j + '0');
-			else
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
