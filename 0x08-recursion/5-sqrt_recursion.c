24eb911201280b561580c67487f249bdae9c2ae9
Abraham <abrahamosazee3@gmail.com>
2022-06-22 14:34:25 -0700

    prime numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _sqrt_recursion - return the natural sqaure root of a number
+ * @n: argument to the function
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value
+ */
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	x = sqrt(n);
+	if (n != x * x)
+		return (-1);
+	
+}
