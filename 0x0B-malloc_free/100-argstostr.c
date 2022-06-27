0f2c3f42218d1e85530453d4bbe3996fb75e9cf0
Abraham <abrahamosazee3@gmail.com>
2022-06-27 13:03:47 -0700

    fix betty error messages

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,10 +12,13 @@ char *argstostr(int ac, char **av)
 {
 	int i, j, k, count = 0;
 	char *ptr;
-	
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	/* count the number of char in each string of argument pass to the program */
+	/**
+	 * count the number of char in each string of argument
+	 * pass to the program
+	 */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -23,17 +26,21 @@ char *argstostr(int ac, char **av)
 		count++;
 	}
 	count++;
-	/* Allocating memory for the total number of char and new line for each words */
+	/**
+	 * Allocating memory for the total number of char
+	 * and new line for each words
+	 */
 	ptr = (char *)malloc(count * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	k = 0; i = 0;
+	k = 0;
+	i = 0;
 	for ( ; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			ptr[k++] = av[i][j];
-                }
+		}
 		ptr[k++] = '\n';
 	}
 	ptr[k] = '\0';
