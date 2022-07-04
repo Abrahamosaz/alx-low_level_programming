ebd9eaf46233f746ff3990a25d592780b5125815
Abraham <abrahamosazee3@gmail.com>
2022-07-04 08:13:43 -0700

    allocate memory for struct dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - pointer to a new allocated memory
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: return a pointer to a new memory allocated
+ *
+ * Return: return dog_t pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	} else
+	{
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+		return (ptr);
+	}
+	return (NULL);
+}
