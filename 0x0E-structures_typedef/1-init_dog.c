92f0edc998f47ff5463935cf1785e2b72b0c240e
Abraham <abrahamosazee3@gmail.com>
2022-07-04 06:57:12 -0700

    initialize dog struct with values

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initialize value for the struct dog
+ * @d: pointer to the struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: pointer to the owner of the dog
+ *
+ * Descriprion: initialize the value of the struct members
+ *
+ * Return: return void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
