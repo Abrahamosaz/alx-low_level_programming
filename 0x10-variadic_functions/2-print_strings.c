071a915903b20d9d0b41f9333ddea818e4ae8236
Abraham <abrahamosazee3@gmail.com>
2022-07-07 08:10:59 -0700

    "dealling with string args"

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - print strings
+ * @separator: string separator
+ * @n: total number of argument
+ *
+ * Description: return the required result
+ *
+ * Return: return void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	size_t i;
+	char *ptr;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(args, char *);
+		if (ptr == NULL)
+			printf("%s", "(nill)");
+		if (separator == NULL)
+		{
+			printf("%s", ptr);
+		} else
+		{
+			printf("%s", ptr);
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("%c", '\n');
+	va_end(args);
+}
