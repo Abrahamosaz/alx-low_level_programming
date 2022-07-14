aaca2a5c09e7b020ed899fcf70ca7268e97a1367
Abraham <abrahamosazee3@gmail.com>
2022-07-14 09:25:20 -0700

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * add_node - add to the beginning of linked list
+ * @head: head pointer
+ * @str: const string
  *
+ * Description: return the required result
+ *
+ * Return: return a pointer
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = NULL;
 	size_t len = 0;
 
-	if (*head == NULL && str == NULL)
+	if (head == NULL && str == NULL)
 		return (NULL);
 	ptr = malloc(sizeof(list_t));
 	ptr->str = strdup(str);
