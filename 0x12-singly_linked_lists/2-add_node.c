01163ef148cb0f8080aeac0efb6add79bfc1490c
Abraham <abrahamosazee3@gmail.com>
2022-07-14 09:44:45 -0700

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len] != '\0')
 		len++;
 	/* Allocating memory to duplicate the string */
-	new_string = malloc(sizeof(char) * len);
+	new_string = malloc(sizeof(char) * len + 1);
 	if (new_string == NULL)
 		return (NULL);
 	new_string = strdup(str);
