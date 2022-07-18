9cb2facaaa9286e011e46bca5b6f713c7458876d
Abraham <abrahamosazee3@gmail.com>
2022-07-18 08:09:28 -0700

    free linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - free a listint_t linked list
+ * @head: pointer to the first element
+ *
+ * Description: free the required memories
+ *
+ * Return: return void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *free_nodes = head;
+
+	while (head != NULL)
+	{
+		free_nodes = head;
+		free(free_nodes);
+		head = free_nodes->next;
+	}
+}
