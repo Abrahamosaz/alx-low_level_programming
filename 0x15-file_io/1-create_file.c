78693f11b5807fa951734ab263e5e0cc56bb56e5
omorisiagbon abraham <abrahamosazee3@gmail.com>
2022-07-25 22:35:18 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
+	{
+		write(1, "fails", 5);
 		return (-1);
+	}
 	if (text_content)
 	{
 		i = 0;
@@ -26,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		value = write(fd, text_content, i);
 		if (value == -1)
 		{
-			write(STDOUT_FILENO, "fails", 5);
+			write(1, "fails", 5);
 			return (-1);
 		}
 	}
