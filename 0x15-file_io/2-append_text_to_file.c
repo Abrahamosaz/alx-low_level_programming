11d556d89cd2a99e247f9d3f3d4d4386f0b44185
omorisiagbon abraham <abrahamosazee3@gmail.com>
2022-07-25 19:57:47 +0000

    dealling with files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * append_text_to_file - append text to the end of file
+ * @filename: name of file
+ * @text_content: text to be append
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, value;
+	size_t len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND | O_WRONLY, 0600);
+	if (fd < 0)
+		return (-1);
+	len = sizeof(text_content);
+	if (text_content)
+	{
+		value = write(fd, text_content, len);
+		if (value < 0)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+dealling with files system calls
